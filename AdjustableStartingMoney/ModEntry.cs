06e069566a928c3b8c8f74d34da498e77ebcab4a
CJVeto <30154721+CJVeto@users.noreply.github.com>
2026-06-01 07:44:56 -0400

    Add core starting money logic on SaveLoaded

--- AdjustableStartingMoney/ModEntry.cs
+++ AdjustableStartingMoney/ModEntry.cs
@@ -1,17 +1,27 @@
 ﻿using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewValley;
 
 namespace VetoCV.AdjustableStartingMoney
 {
+
     internal class ModEntry : Mod
     {
+        private ModConfig Config = null!;
         public override void Entry(IModHelper helper)
         {
+            Config = helper.ReadConfig<ModConfig>();
+            
             helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
         }
 
         private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
         {
+            if (Game1.stats.DaysPlayed == 1)
+            {
+                Game1.player.Money = Config.StartingMoney;
+                Monitor.Log($"Applied starting money: {Config.StartingMoney}g", LogLevel.Info);
+            }
         }
 
     }
