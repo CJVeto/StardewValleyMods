2729773a197d2713cf04ee32b96cec7aeab75d45
CJVeto <30154721+CJVeto@users.noreply.github.com>
2026-06-01 08:53:34 -0400

    Add GenericModConfigMenu integration

---
+++ AdjustableStartingMoney/IGenericModConfigMenuApi.cs
@@ -0,0 +1,10 @@
+﻿using StardewModdingAPI;
+using System;
+
+public interface IGenericModConfigMenuApi
+{
+    void Register(IManifest mod, Action reset, Action save, bool titleScreenOnly = false);
+    void AddTextOption(IManifest mod, Func<string> getValue, Action<string> setValue,
+        Func<string> name, Func<string>? tooltip = null, string[]? allowedValues = null,
+        Func<string, string>? formatAllowedValue = null, string? fieldId = null);
+}
--- AdjustableStartingMoney/ModEntry.cs
+++ AdjustableStartingMoney/ModEntry.cs
@@ -1,5 +1,6 @@
 ﻿using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewModdingAPI.Integrations.GenericModConfigMenu;
 using StardewValley;
 
 namespace VetoCV.AdjustableStartingMoney
@@ -11,10 +12,37 @@ namespace VetoCV.AdjustableStartingMoney
         public override void Entry(IModHelper helper)
         {
             Config = helper.ReadConfig<ModConfig>();
-            
+
+            helper.Events.GameLoop.GameLaunched += OnGameLaunched;
             helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
 
             Monitor.Log($"Loaded config: StartingMoney = {Config.StartingMoney}g", LogLevel.Trace);
+        }
+
+        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>
+                ("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => this.Config = new ModConfig(),
+                save: () =>
+                {
+                    this.Helper.WriteConfig(this.Config);
+                    Monitor.Log($"Config saved: StartingMoney = {this.Config.StartingMoney}g", LogLevel.Info);
+                }
+            );
+
+            configMenu.AddTextOption(
+                mod: this.ModManifest,
+                name: () => this.Helper.Translation.Get("config.startingMoney.name"),
+                tooltip: () => this.Helper.Translation.Get("config.startingMoney.tooltip"),
+                getValue: () => this.Config.StartingMoney.ToString(),
+                setValue: value => this.Config.StartingMoney = int.TryParse(value, out int result) && result >= 0 ? result : this.Config.StartingMoney
+            );
         }
 
         private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
